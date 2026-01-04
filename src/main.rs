fd97ce9bda7b7f4fb72711f015dc120252d314d5
Francesco Forcher <francesco.forcher@gmail.com>
2026-01-04 23:50:15 +0100

    basic app logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use chrono::{SecondsFormat, Utc};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut counter: u64 = 0;
+
+    loop {
+        thread::sleep(Duration::from_secs(1));
+        counter += 1;
+        let timestamp = Utc::now().to_rfc3339_opts(SecondsFormat::Secs, true);
+        println!(r#"{{"timestamp":"{}","counter":"{}"}}"#, timestamp, counter);
+    }
 }
